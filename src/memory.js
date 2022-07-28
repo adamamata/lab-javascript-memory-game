8e5bb8925b07892b332ef028c0ae9bc90c9d57c7
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-28 16:33:19 +0200

    finished memory.js

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
+  //FISHER YATES ALGORITHM
   shuffleCards() {
-    // ... write your code here
+    const cardsArray = this.cards;
+    if (this.cards){
+      for (let i = cardsArray.length - 1; i > 0; i--){ //Loop through the array starting at the end
+        let randomIndex = Math.floor(Math.random() * (i + 1)); //Generating a random number (max = array length)
+        let oldElement = cardsArray[i]; //old element = element that the loop is sitting on
+        cardsArray[i] = cardsArray[randomIndex]; //swapping old element with the element whose index is randomIndex
+        cardsArray[randomIndex] = oldElement; 
+      }
+      return cardsArray;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++; 
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    return this.pairsGuessed === (this.cards.length / 2) ? true : false;
   }
 }
